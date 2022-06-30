7e4a6196b717ac8919cc2ed85887e34ef6c8b21d
ZacBurnett <zachary710@revature.net>
2022-06-30 09:57:25 -0700

    Adding changes to .tf files

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -13,18 +13,11 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "worker-group-1"
+      name                          = "worker-main"
       instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
+      additional_userdata           = "echo foo bar" 
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 3
     },
   ]
 }
